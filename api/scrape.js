105f64b02a1dd28a8ef4302330b9a80afd2ffbfc
sanjay <sanjay13649@gmail.com>
2025-11-13 22:20:21 +0530

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -1,6 +1,17 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
+  // ---- CORS FIX ----
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Handle preflight
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  // -------------------
+
   const playlistId = req.query.playlist;
 
   if (!playlistId) {
